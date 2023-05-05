9e0bba8dc5eeca80df6101ad1f91218842eebcd4
idoshitrit <ido7749@Gmail.com>
2023-05-05 13:57:10 +0300

    add flag object

---
+++ Assets/Scripts/Global/FlagSystem.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlagSystem : MonoBehaviour
+{
+    public GameObject flagObject;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
