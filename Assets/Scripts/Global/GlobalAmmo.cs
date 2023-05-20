40e076cb640ccc22328a244aa955c2c1e2c1fb8b
Roie96 <roie26296@gmail.com>
2023-05-20 19:03:56 +0300

    added global text

--- Assets/Scripts/Global/GlobalAmmo.cs
+++ Assets/Scripts/Global/GlobalAmmo.cs
@@ -6,9 +6,18 @@ using TMPro;
 public class GlobalAmmo : MonoBehaviour
 {
     public GameObject ammoText;
+    public GameObject barricadeText;
+    public BuildSystem buildSystem; // Add a public variable to hold the reference to the BuildSystem class
+
+       void Start()
+    {
+        // Assign the reference to the BuildSystem class
+        buildSystem = FindObjectOfType<BuildSystem>();
+    }
 
     void Update()
     {
+        barricadeText.GetComponent<TextMeshProUGUI>().text = "BARRICADE: "+buildSystem.getCurrAmmoBarricade();
         ammoText.GetComponent<TextMeshProUGUI>().text = "AMMO: " + Weapon_holder.GunScript.getCurrMagAmmo()+"/"+Weapon_holder.GunScript.getCurrAmmo();
     }
 }
